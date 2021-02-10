03897506be5372a8426a52f97d84681358d1761c
Bobby Sluka <bsluka@albany.edu>
2021-02-10 14:24:19 -0500

    Created shell file with operation commands and to also be used for the lab due monday

---
+++ args_lab.sh
@@ -0,0 +1,57 @@
+#! /bin/bash
+
+echo "The number of arguments is $#"
+
+echo "The first argument is $1"
+
+echo "Argument \$2 is $2"
+
+echo "Argument \$0 is $0"
+
+echo "Argument \$9 is $9"
+
+echo "All arguments are accessed with \$* : $*"
+
+# access the arguments
+
+# add the two numbers
+let "sum=$var1 + $var2"
+# print the sum
+echo "sum \= $sum"
+# multiply the two numbers
+let "product=$var1 * $var2"
+# print the product
+echo "product \= $product"
+
+
+
+# BFOR 206 Lab
+## Class 2-2
+
+# Task Description
+Create a script that accepts two numbers as arguments
+and prints the sum and product of the two numbers.
+
+# Input
+**Arguments:** two numbers
+
+# Output
+**Terminal:** The sum and product of the two numbers.
+
+# Test Cases
+
+```
+# Input: 3 4
+# Output: sum = 7
+          product = 12
+
+# Input:  9 9
+# Output: sum = 18
+          product = 81
+
+```
+
+# Submission instructions
+When you are finished, submit two screenshots:
+1.  A screenshot of your code.
+2.  A screenshot of correct output.
