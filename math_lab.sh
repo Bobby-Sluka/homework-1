a2b5e6389a994edbe5521ced15cabc9275ee3eed
Bobby Sluka <bsluka@albany.edu>
2021-02-14 14:47:00 -0500

    Completed lab 2-2 due mon 2/15

---
+++ math_lab.sh
@@ -0,0 +1,9 @@
+#add the two numbers
+echo "Numbers used:$1 and $2"
+let "sum=$1 + $2"
+# print the sum
+echo "The sum = $sum"
+# multiply the two numbers
+let "product=$1 * $2"
+# print the product
+echo "The product = $product"
