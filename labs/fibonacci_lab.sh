ef630333771111f265638df7304e770a88c287c1
Bobby Sluka <bsluka@albany.edu>
2021-02-21 16:03:50 -0500

    Made changes, added loop and created array

--- labs/fibonacci_lab.sh
+++ labs/fibonacci_lab.sh
@@ -1,5 +1,14 @@
 #! /bin/bash
 
 # lab fibonacci sequence
+#use java loop, counter = users input to count
 
-
+array=( 0 1 )
+echo "The input is $1"
+for (( i=1; i<=$1; i++ ))
+do
+	let "add1=${array[$i-1]}"
+	echo $add
+	array+=( ${array[$i-1]} + ${array[$i]})
+done
+echo ${array[@]}
