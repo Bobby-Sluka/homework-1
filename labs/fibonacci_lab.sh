2e1093f329d6ac1ec76a2ccf030e5da4853e7bbb
Bobby Sluka <bsluka@albany.edu>
2021-02-22 14:23:20 -0500

    Completed Fibonacci lab

--- labs/fibonacci_lab.sh
+++ labs/fibonacci_lab.sh
@@ -5,12 +5,18 @@
 
 array=( 0 1 )
 echo "The input is $1"
-for (( i=1; i<=$1; i++ ))
-do
-	let "var=$i - 1"
-	let "add1=${array[$var]}"
-	let "add2=${array[$i]}"
-	let "sum=$add1 + $add2"
-	array+=( $sum )
-done
+if [ $1 -le 0 ]
+	then echo "0"
+elif [ $1 -eq 1 ]
+	then echo "0 1"
+else
+	for (( i=1; i<$1; i++ ))
+	do
+		let "var=$i - 1"
+		let "add1=${array[$var]}"
+		let "add2=${array[$i]}"
+		let "sum=$add1 + $add2"
+		array+=( $sum )
+	done
 echo ${array[@]}
+fi
