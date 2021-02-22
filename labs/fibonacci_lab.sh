358118444beb1bb627f9d0c126146898e48ec550
Bobby Sluka <bsluka@albany.edu>
2021-02-21 20:46:48 -0500

    Now have fibonacci sequence working

--- labs/fibonacci_lab.sh
+++ labs/fibonacci_lab.sh
@@ -7,8 +7,10 @@ array=( 0 1 )
 echo "The input is $1"
 for (( i=1; i<=$1; i++ ))
 do
-	let "add1=${array[$i-1]}"
-	echo $add
-	array+=( ${array[$i-1]} + ${array[$i]})
+	let "var=$i - 1"
+	let "add1=${array[$var]}"
+	let "add2=${array[$i]}"
+	let "sum=$add1 + $add2"
+	array+=( $sum )
 done
 echo ${array[@]}
