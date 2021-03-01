be677e8dbe28f6476e6bb1f6fa992c3475fa9ba0
Bobby Sluka <bsluka@albany.edu>
2021-03-01 15:00:07 -0500

    modified example file used for lab 4-2 final submission. Read/write and ping

--- input_script.sh
+++ input_script.sh
@@ -2,19 +2,20 @@
 
 # read input from file
 # reads into variable called $input
-read input < input.txt
+#read input < input.txt
 
 # make sure file reads properly
 # by showing contents of the 
 # input variable
-echo "\$input is $input"
+#echo "\$input is $input"
 
 printf "\n##########################\n\n" >> output.log
 
 # put the date and time into the log
 echo "The script is running at " `date` >> output.log
+echo >> output.log
+#### print and store the results
 
-#### pint google and store the results
-
-ping -c3 $input >> output.log
+ping -c3 "youtube.com" >> output.log
+echo >> output.log
 echo "Done." >> output.log
