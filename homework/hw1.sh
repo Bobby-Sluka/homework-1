02b76f09a5a492e027a4acbbda25a7a762432fa7
Bobby Sluka <bsluka@albany.edu>
2021-03-22 21:08:12 -0400

    made progress with parsing ping output, still need to get '4 received' parsed to retrieve the integer

--- homework/hw1.sh
+++ homework/hw1.sh
@@ -8,13 +8,23 @@ lines=`cat $input`
 for line in $lines
 do
 	pg=$(ping $line -c4 | grep "packet loss")
-	echo "$pg" >> ping.txt 
+	#echo "$pg" > ping.txt
+	echo "$pg" | cut -d "%" -f1 > ping.txt
+	value="$pg" | cut -d "," -f2
+	final="value" | cut -d " " -f1
+#	echo "final is $final"
+	echo "$value"
+	#if [ "$final" == "4" ]
+	#	then echo "online because $value = 4"
+	#else
+	#	echo "offline because $value = 0"
+	##fi  
 done
-pingtext=ping.txt
-parse=`cat $pingtext`
-for p in parse
-do
-	#echo “$p” | cut -d% -f1
-	echo "$p"
-done
+#pingtext=ping.txt
+#parse=`cat $pingtext`
+#for p in parse
+#do
+#	#echo “$p” | cut -d% -f1
+#	echo "$p"
+#done
 
